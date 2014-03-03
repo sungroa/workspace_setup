ceb054b04949bce0a1d69a051c5cdfc9a13d4486
sungroa <sungroa@gmail.com>
2014-03-02 19:10:23 -0800

    changed copy_out and load_here to create backups

--- load_here.sh
+++ load_here.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #Loads all the appropriate files into this repo.
 ls -A > temp_bash_dir
+rm -rf ~/.backup_my_personal_settings
+mkdir ~/.backup_my_personal_settings
 while read myline
 do
     if [ "$myline" != "load_here.sh" ] && [ "$myline" != "temp_bash_dir" ] && [ "$myline" != ".git" ] && [ "$myline" != "copy_out.sh" ]
     then
-    cp ~/$myline .
+        cp $myline ~/.backup_my_personal_settings
+        cp ~/$myline .
     fi
 done < "temp_bash_dir"
 
